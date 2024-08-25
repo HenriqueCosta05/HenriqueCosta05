08b8ce431381e48ce033b5d8c87ce50bd4ec19e6
HenriqueCosta05 <henrique.costa10@fatec.sp.gov.br>
2024-08-25 19:10:03 -0300

    preparando para deploy pt.2

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
 import React, { useContext } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 import { ThemeContext } from './contexts/ThemeContext';
-import { Main, BlogPage, ProjectPage } from './pages'
-import { BackToTop } from './components'
-import ScrollToTop from './utils/ScrollToTop'
+import { Main, BlogPage, ProjectPage } from './pages';
+import { BackToTop } from './components';
+import ScrollToTop from './utils/ScrollToTop';
 
-import './App.css'
+import './App.css';
 
 function App() {
-
   const { theme } = useContext(ThemeContext);
 
   return (
     <div className="app">
       <Router>
-        <ScrollToTop/>
-        <Switch>
-          <Route path="/" exact component={Main} />
-          <Route path="/blog" exact component={BlogPage} />
-          <Route path="/projects" exact component={ProjectPage} />
-
-          <Redirect to="/" />
-        </Switch>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/projects" element={<ProjectPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </Router>
       <BackToTop />
     </div>
