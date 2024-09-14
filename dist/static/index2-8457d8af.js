7591bca080e4a6d6a623cc73b386da7f36bd03bf
HenriqueCosta05 <Henrique.b.costa9090@gmail.com>
2024-09-13 21:01:57 -0300

    bufix: cors e sanity

--- dist/static/index2-8457d8af.js
+++ dist/static/index2-8457d8af.js
@@ -1,4 +1,4 @@
-import{h as b,al as f,j as a,Y as P,aq as g,x as B,ar as L,r as v,U as w,as as k,$ as T,at as A,e as C,an as S,Z as _,ao as W,W as D,B as H,ap as E}from"./sanity-4e6b30f6.js";const G=b.hr`
+import{h as b,al as f,j as a,Y as P,aq as g,x as B,ar as L,r as v,U as w,as as k,$ as T,at as A,e as C,an as S,Z as _,ao as W,W as D,B as H,ap as E}from"./sanity-0a0dd8a6.js";const G=b.hr`
   background-color: var(--card-border-color);
   height: 1px;
   margin: 0;
--- sanity.config.ts
+++ sanity.config.ts
@@ -2,12 +2,11 @@ import { defineConfig, isDev } from "sanity";
 import { structureTool } from "sanity/structure";
 import schemas from "./sanity/schemas";
 import { visionTool } from "@sanity/vision";
+import { clientConfig } from "./sanity/config/client-config";
 
 const config = defineConfig({
+  ...clientConfig,
   projectId: "fmcssjtc",
-  dataset: "production",
-  title: "Henrique Costa Portfolio Website",
-  apiVersion:  "2021-03-25",
   basePath: "/admin",
   plugins: isDev ? [structureTool(), visionTool()] : [structureTool()],
   schema: { types: schemas },
