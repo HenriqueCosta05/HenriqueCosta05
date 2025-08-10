e3804157181d87237fc163a1460b91ff8b1cd795
HenriqueCosta05 <henriquebenedictocostadev@gmail.com>
2025-08-10 19:26:07 -0300

    bugfix: current env implementation

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
 import schemas from "./sanity/schemas";
 
+const currentEnv = process.env.NODE_ENV || "development";
+
 const config = defineConfig({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string,
-  dataset: process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET as string,
+  projectId: currentEnv === "development" ? process.env.NEXT_PUBLIC_SANITY_PROJECT_ID as string : process.env.SANITY_PROJECT_ID as string,
+  dataset: currentEnv === "development" ? process.env.NEXT_PUBLIC_SANITY_PROJECT_DATASET as string : process.env.SANITY_PROJECT_DATASET as string,
   title: "Henrique Costa's Portfolio",
   plugins: [structureTool()],
   schema: { types: schemas },
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import articles from "./articles";
+import experiences from "./experiences";
+import projects from "./projects";
+
+const schemas = [articles, projects, experiences]
+export default schemas
